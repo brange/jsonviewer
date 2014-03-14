4edc8f3b9540cf157b1814284323f261d49ecfb6
Magnus Brange <magnus.brange@telavox.se>
2014-03-14 23:29:22 +0100

    Maybe I should add the files to git..

---
+++ src/main/java/se/brange/jsonviewer/json/JSONHelper.java
@@ -0,0 +1,75 @@
+package se.brange.jsonviewer.json;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class JSONHelper {
+
+    public enum JSONValueType {
+        STRING,
+        NUMBER,
+        INTEGER,
+        OBJECT,
+        BOOLEAN,
+        ARRAY,
+        UNKOWN
+    }
+
+    public static JSONValueType getType(Object object) {
+        if (object instanceof JSONValue) {
+            object = ((JSONValue)object).getValue();
+        }
+        if (object instanceof String) {
+            return JSONValueType.STRING;
+        }
+        if (object instanceof Boolean) {
+            return JSONValueType.BOOLEAN;
+        }
+        if (object instanceof Float || object instanceof Double) {
+            return JSONValueType.NUMBER;
+        }
+        if (object instanceof Integer) {
+            return JSONValueType.INTEGER;
+        }
+        if (object instanceof JSONHolder || object instanceof JSONObject || object instanceof JSONArray) {
+            object = convertFromJSONHolder(object);
+            if (object instanceof JSONObject) {
+                return JSONValueType.OBJECT;
+            }
+            if (object instanceof JSONArray) {
+                return JSONValueType.ARRAY;
+            }
+        }
+
+        return JSONValueType.UNKOWN;
+    }
+
+    public static String getStringValue(Object object) {
+        switch (getType(object)) {
+            case STRING: return "String";
+            case NUMBER: return "Number";
+            case INTEGER: return "Integer";
+            case BOOLEAN: return "Boolean";
+            case OBJECT: return "Object (" + (getSize(object)) + ")";
+            case ARRAY: return "Array (" + (getSize(object)) + ")";
+            default: return "Unkown (" + object.getClass() + ")";
+        }
+    }
+
+    private static int getSize(Object object) {
+        object = convertFromJSONHolder(object);
+        if (object instanceof JSONObject) {
+            return ((JSONObject)object).length();
+        } else if (object instanceof JSONArray) {
+            return ((JSONArray)object).length();
+        }
+        return 0;
+    }
+
+    public static Object convertFromJSONHolder(Object o) {
+        if (o instanceof JSONHolder) {
+            return ((JSONHolder)o).getRawObject();
+        }
+        return o;
+    }
+}
---
+++ src/main/java/se/brange/jsonviewer/json/JSONValue.java
@@ -0,0 +1,55 @@
+package se.brange.jsonviewer.json;
+
+import org.json.JSONArray;
+
+public class JSONValue {
+    private String key;
+    private Object value;
+    private Object parent;
+    private Integer index;
+
+    public JSONValue(Integer index, Object value, Object parent) {
+        this.index = index;
+        this.value = value;
+        this.parent = parent;
+    }
+
+    public JSONValue(String key, Object value, Object parent) {
+        this.key = key;
+        this.value = value;
+        this.parent = parent;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        System.out.println("value.getClass() = " + value.getClass());
+        this.value = value;
+    }
+
+    public Object getParent() {
+        return parent;
+    }
+
+    public Integer getIndex() {
+        return index;
+    }
+
+    public JSONHelper.JSONValueType getType() {
+        return JSONHelper.getType(value);
+    }
+
+    @Override
+    public String toString() {
+        if (parent != null && parent instanceof JSONArray) {
+            return Integer.toString(index);
+        }
+        return key;
+    }
+}
