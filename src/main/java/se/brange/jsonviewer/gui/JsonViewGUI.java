42ab33a7856c7911ba74396d65ffd3d275d13f36
Magnus Brange <magnus.brange@telavox.se>
2014-03-16 08:48:20 +0100

    Some errorhandling
    Showing the error message in the tooltip of inputLabel, and making the foreground color red.
    
    Also trying to highlight the error in the JTextPane.

--- src/main/java/se/brange/jsonviewer/gui/JsonViewGUI.java
+++ src/main/java/se/brange/jsonviewer/gui/JsonViewGUI.java
@@ -1,23 +1,24 @@
 package se.brange.jsonviewer.gui;
 
-import java.awt.Component;
+import java.awt.Color;
 import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.EventObject;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.JTextArea;
+import javax.swing.JTextPane;
 import javax.swing.JViewport;
-import javax.swing.event.CellEditorListener;
-import javax.swing.table.TableCellEditor;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.netbeans.swing.outline.DefaultOutlineModel;
 import org.netbeans.swing.outline.Outline;
@@ -27,7 +28,7 @@ import se.brange.jsonviewer.json.JSONHolder;
 public class JsonViewGUI extends JFrame {
 
     // GUI components that are added via JsonViewGUI.form (IntelliJ GUI builder)
-    private JTextArea inputArea;
+    private JTextPane inputArea;
     private JLabel outputLabel;
     private JLabel inputLabel;
     private JScrollPane outputScrollPane;
@@ -55,7 +56,9 @@ public class JsonViewGUI extends JFrame {
 
     private void createUIComponents() {
         if (inputArea==null) {
-            inputArea = new JTextArea("hej1");
+            inputArea = new JTextPane();
+            Style style = inputArea.addStyle("DEFAULT", null);
+            StyleConstants.setForeground(style, Color.BLACK);
             button1 = new JButton();
             button2 = new JButton();
             button3 = new JButton();
@@ -99,17 +102,34 @@ public class JsonViewGUI extends JFrame {
                 public void keyReleased(KeyEvent keyEvent) {
                     String json = inputArea.getText();
                     if (json != null) {
-                        if (json.startsWith("{")) {
-                            JSONObject jsonObject = new JSONObject(json);
-                            System.out.println("jsonObject = " + jsonObject.toString(2));
-                            updateOutline(jsonObject);
-                        } else if (json.startsWith("[")) {
-                            JSONArray jsonArray = new JSONArray(json);
-                            System.out.println("jsonArray = " + jsonArray.toString(2));
-                            updateOutline(jsonArray);
-                        } else {
-                            System.out.println("error, not a JSONObjec/JSONArray '" + json + "'.");
-                            // TODO: Warn, not a JSONObject and not a JSONArray.
+                        clearErrorMessageAndStyle();
+                        try {
+                            if (json.startsWith("{")) {
+                                JSONObject jsonObject = new JSONObject(json);
+                                updateOutline(jsonObject);
+                            } else if (json.startsWith("[")) {
+                                JSONArray jsonArray = new JSONArray(json);
+                                updateOutline(jsonArray);
+                            } else {
+                                inputLabel.setForeground(Color.RED);
+                                inputLabel.setToolTipText("Input is not a JSON object or array.");
+                            }
+                        } catch (JSONException jsonException) {
+                            String message = jsonException.getMessage();
+                            inputLabel.setToolTipText(message);
+                            inputLabel.setForeground(Color.RED);
+                            Pattern pattern = Pattern.compile("(.*)at ([0-9]+) \\[character ([0-9]+) line ([0-9]+)\\](.*)");                            Matcher matcher = pattern.matcher(message);
+                            if (matcher != null && matcher.matches()) {
+                                String absoluteCharacter = matcher.group(2);
+                                Style style = inputArea.addStyle("Red", null);
+                                StyleConstants.setForeground(style, Color.RED);
+                                inputArea.getStyledDocument().setCharacterAttributes(
+                                    Integer.parseInt(absoluteCharacter)-2,
+                                    3,
+                                    inputArea.getStyle("Red"),
+                                    true
+                                );
+                            }
                         }
                     }
                 }
@@ -118,15 +138,25 @@ public class JsonViewGUI extends JFrame {
         }
     }
 
+    private void clearErrorMessageAndStyle() {
+        String json = inputArea.getText();
+        inputLabel.setForeground(null);
+        inputLabel.setToolTipText(null);
+        inputArea.getStyledDocument().setCharacterAttributes(0, json.length(), inputArea.getStyle("DEFAULT"), true);
+    }
+
     private void updateOutline(Object jsonObject) {
         this.currentJson = jsonObject;
         OutlineModel outlineModel =  DefaultOutlineModel.createOutlineModel(new JsonTreeModel(new JSONHolder("ROOT", jsonObject)), new JsonRowModel(this), false, "Key");
-        Outline rootOutline = new Outline(outlineModel);;
+        Outline rootOutline = new Outline(outlineModel);
         viewPort.add(rootOutline);
     }
 
 
     public void updateTextAreaText() {
         inputArea.setText(((JSONObject)currentJson).toString(4));
+        if (inputLabel != null) {
+            clearErrorMessageAndStyle();
+        }
     }
 }
