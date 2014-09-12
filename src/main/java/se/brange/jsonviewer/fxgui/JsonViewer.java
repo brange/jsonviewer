4b692a0117bb6bf72dce642c9fe316a74cc31bb2
Magnus Brange <magnus.brange@telavox.se>
2014-09-12 22:17:19 +0200

    Adding controls for InputArea and CopyButton
    Every change in the Input area outputs in the JSON tree view.
    The copy button works (woho..)

--- src/main/java/se/brange/jsonviewer/fxgui/JsonViewer.java
+++ src/main/java/se/brange/jsonviewer/fxgui/JsonViewer.java
@@ -1,17 +1,28 @@
 package se.brange.jsonviewer.fxgui;
 
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeTableView;
 import javafx.stage.Stage;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import se.brange.jsonviewer.controllers.Searcher;
 import se.brange.jsonviewer.controllers.TreeBuilder;
+import se.brange.jsonviewer.json.JSONHolder;
 
 public class JsonViewer extends Application {
 
@@ -24,6 +35,8 @@ public class JsonViewer extends Application {
 
     private Searcher searcher;
     private TreeBuilder treeBuilder;
+    private Button copyButton;
+    private CheckBox prettyCopy;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -50,11 +63,72 @@ public class JsonViewer extends Application {
         treeTableView = (TreeTableView) scene.lookup("#tableView");
         searcher = new Searcher(this);
         treeBuilder = new TreeBuilder(this);
+
+        inputArea = (TextArea) scene.lookup("#inputArea");
+        setupInputArea();
+
+        copyButton = (Button) scene.lookup("#copyButton");
+        prettyCopy = (CheckBox) scene.lookup("#prettyCopy");
+        setupCopyButton();
+    }
+
+    private void setupCopyButton() {
+        copyButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                JSONHolder value = (JSONHolder) treeTableView.getRoot().getValue();
+
+                int indent = 0;
+                if (prettyCopy.isSelected()) {
+                    indent = 2;
+                }
+
+                String text;
+                if (value.isJSONObject()) {
+                    text = value.getJSONObject().toString(indent);
+                } else if (value.isJSONArray()) {
+                    text = value.getJSONArray().toString(indent);
+                } else {
+                    text = "Error...";
+                }
+
+                StringSelection stringSelection = new StringSelection(text);
+                Clipboard clpbrd = Toolkit.getDefaultToolkit().getSystemClipboard ();
+                clpbrd.setContents(stringSelection, null);
+            }
+        });
+    }
+
+    private void setupInputArea() {
+        inputArea.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String oldText, String newText) {
+                if (newText.startsWith("{")) {
+                    // JSONObject
+                    try {
+                        JSONObject jsonObject = new JSONObject(newText);
+                        setRootJson(jsonObject);
+                    } catch (JSONException e) {
+                        setRootJson(new JSONObject().put("error", "Invalid JSONObject"));
+                    }
+                } else if (newText.startsWith("[")) {
+                    // JSON array.
+                    try {
+                        JSONArray array = new JSONArray(newText);
+                        setRootJson(array);
+                    } catch (JSONException e) {
+                        setRootJson(new JSONObject().put("error", "Invalid JSONArray"));
+                    }
+                } else {
+                    setRootJson(new JSONObject().put("error", "Invalid JSON input"));
+                }
+            }
+        });
     }
 
 
-    private void setRootJson(JSONObject testObject) {
-        root = treeBuilder.constructRoot(testObject);
+    private void setRootJson(Object jsonObject) {
+        root = treeBuilder.constructRoot(jsonObject);
         treeTableView.setRoot(root);
     }
 
